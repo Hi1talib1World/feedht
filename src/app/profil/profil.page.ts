cb76a0ac549a4d268a2ad565d55b6837496f34d3
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-07-20 14:27:31 +0100

    Update profil.page.ts

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.page.scss'],
 })
 export class ProfilPage implements OnInit {
+  fbava:any;
+  id:any;
+modi:boolean;
+ifmodi:boolean;
+hidebutton:boolean;
+name:any;
+data:AfoObjectObservable<any>;
+img:any;
+ loading: Loading;
+pic:any;
+ava:any;
+sex:any;
+male1:boolean;
+female1:boolean;
+stat:any;
+gene:boolean;
 
   constructor() { }
 
