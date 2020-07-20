6e11adcc38d002a594b107a1278c4f65509353db
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-07-20 13:34:40 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,8 +73,7 @@ export class AppComponent implements OnInit {
   }
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      
     });
   }
 
