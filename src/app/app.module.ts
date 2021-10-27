63a9432b9564bcf5a280b4eacebf6271c4474331
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-10-27 14:01:56 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
