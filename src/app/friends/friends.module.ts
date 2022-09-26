d29c9517c0997d5eb154d029cbe787ce50943af1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-26 02:40:56 +0100

    Update friends.module.ts

--- src/app/friends/friends.module.ts
+++ src/app/friends/friends.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FriendsPageRoutingModule } from './friends-routing.module';
-
 import { FriendsPage } from './friends.page';
 
 @NgModule({
