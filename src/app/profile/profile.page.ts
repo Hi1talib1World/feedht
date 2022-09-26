f5f124f6cfb86d6a36940f2f75b6c8b78a745ec5
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-26 02:58:56 +0100

    Update profile.page.ts

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../services/api.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  friends: any;
+
+  constructor(
+      private apiService: ApiService
+  ) { }
 
   ngOnInit() {
+    this.loadPhotos();
+  }
+
+  loadPhotos() {
+    this.apiService.getFriends().then(result => {
+      this.friends = result;
+    }).catch(error => {
+      console.log('getFriends error', error);
+    });
   }
 
 }
